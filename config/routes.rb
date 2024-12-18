75c8029e15bf2fcbfa0a3aa3fb5bd346d96bcfa2
michaellin1227 <130536515+michaellin1227@users.noreply.github.com>
2024-12-18 16:43:06 +0800

    install_rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "/articles", to: "articles#index"
 
-  get "/hello_world", to:"pages#hello"
+  get "/hello_world", to: "pages#hello"
 
   resources :products
-
 end
